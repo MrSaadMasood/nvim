102ff7e5469f1fb90944ed96349e04468a9e0d8e
MrSaadMasood <mrsaadmasood1@gmail.com>
2024-05-21 00:40:48 +0500

    add tmux navigator and add new normal mode key binding

--- README.md
+++ README.md
@@ -1,9 +1,2 @@
-<<<<<<< HEAD
-# nvim
-repo for nvim configuration
-=======
-# 💤 LazyVim
-
 A starter template for [LazyVim](https://github.com/LazyVim/LazyVim).
 Refer to the [documentation](https://lazyvim.github.io/installation) to get started.
->>>>>>> f0b9eed (initial commit)
--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,3 +2,15 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+
+-- vim.keymap.set("n", "<C-h", ":TmuxNavigateLeft<CR>", { silent = true })
+-- vim.keymap.set("n", "<C-j", ":TmuxNavigateDown<CR>", { silent = true })
+-- vim.keymap.set("n", "<C-k", ":TmuxNavigateUp<CR>", { silent = true })
+-- vim.keymap.set("n", "<C-l", ":TmuxNavigateRight<CR>", { silent = true })
+--
+vim.keymap.set("n", "<C-k>", ":wincmd k<CR>", { silent = true })
+vim.keymap.set("n", "<C-j>", ":wincmd j<CR>", { silent = true })
+vim.keymap.set("n", "<C-h>", ":wincmd h<CR>", { silent = true })
+vim.keymap.set("n", "<C-l>", ":wincmd l<CR>", { silent = true })
+
+vim.api.nvim_set_keymap("i", "<C-n>", "<Esc>", { noremap = true, silent = true })
